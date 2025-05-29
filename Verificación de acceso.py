cf777d6da00fe7dd31913af93a501e82031c4a5c
tremor1724 <148923646+trmor1724@users.noreply.github.com>
2025-05-29 12:46:58 -0500

    Update Verificación de acceso.py

--- Verificación de acceso.py
+++ Verificación de acceso.py
@@ -56,9 +56,9 @@ if img_file_buffer is not None:
     print(prediction)
     if prediction[0][0]>0.3:
       st.header('Abriendo')
-      client1.publish("IMIA","{'gesto': 'Abre'}",qos=0, retain=False)
+      client1.publish("servo","{'gesto': 'Abre'}",qos=0, retain=False)
       time.sleep(0.2)
     if prediction[0][1]>0.3:
       st.header('Cerrando')
-      client1.publish("IMIA","{'gesto': 'Cierra'}",qos=0, retain=False)
+      client1.publish("servo","{'gesto': 'Cierra'}",qos=0, retain=False)
       time.sleep(0.2)  
