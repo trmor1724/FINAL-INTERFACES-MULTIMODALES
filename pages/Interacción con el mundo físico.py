c568eba156287ce9e5f9f6d41322c3210f39e318
tremor1724 <148923646+trmor1724@users.noreply.github.com>
2025-05-29 11:12:26 -0500

    Update Interacción con el mundo físico.py

--- pages/Interacción con el mundo físico.py
+++ pages/Interacción con el mundo físico.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit_autorefresh import st_autorefresh
 import paho.mqtt.client as mqtt
 
 st.set_page_config(page_title="🛰️ Monitor PIR - Casa Inteligente", layout="centered")
@@ -8,17 +9,20 @@ st.title("📡 Movimiento en tiempo real (desde Wokwi)")
 if "movimiento" not in st.session_state:
     st.session_state.movimiento = "Esperando datos..."
 
-# Callback cuando se recibe mensaje
+# Callback cuando se recibe mensaje MQTT
 def on_message(client, userdata, message):
     payload = message.payload.decode("utf-8")
     st.session_state.movimiento = f"📍 Movimiento: **{payload}**"
 
-# Configurar MQTT
+# Configurar cliente MQTT
 client = mqtt.Client()
 client.on_message = on_message
 client.connect("broker.mqttdashboard.com", 1883, 60)
 client.subscribe("casa/movimiento")
 client.loop_start()
 
-# Mostrar estado actual
+# Refrescar cada 2 segundos
+st_autorefresh(interval=2000, key="datarefresh")
+
+# Mostrar el estado actual
 st.markdown(st.session_state.movimiento)
