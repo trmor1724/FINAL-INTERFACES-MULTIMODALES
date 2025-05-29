f86732558f6f8c11f2e07155a7edb7ca340aed02
tremor1724 <148923646+trmor1724@users.noreply.github.com>
2025-05-29 11:18:07 -0500

    Update Interacción con el mundo físico.py

--- pages/Interacción con el mundo físico.py
+++ pages/Interacción con el mundo físico.py
@@ -1,24 +1,76 @@
+import paho.mqtt.client as paho
+import time
 import streamlit as st
-import paho.mqtt.client as mqtt
+import json
+import platform
 
-st.set_page_config(page_title="🛰️ Monitor PIR - Casa Inteligente", layout="centered")
-st.title("📡 Movimiento en tiempo real (desde Wokwi)")
+# Muestra la versión de Python junto con detalles adicionales
+st.write("Versión de Python:", platform.python_version())
 
-# Estado global para el último mensaje
-if "movimiento" not in st.session_state:
-    st.session_state.movimiento = "Esperando datos..."
+values = 0.0
+act1="OFF"
 
-# Callback cuando se recibe mensaje
+def on_publish(client,userdata,result):             #create function for callback
+    print("el dato ha sido publicado \n")
+    pass
+
 def on_message(client, userdata, message):
-    payload = message.payload.decode("utf-8")
-    st.session_state.movimiento = f"📍 Movimiento: **{payload}**"
+    global message_received
+    time.sleep(2)
+    message_received=str(message.payload.decode("utf-8"))
+    st.write(message_received)
 
-# Configurar MQTT
-client = mqtt.Client()
-client.on_message = on_message
-client.connect("broker.mqttdashboard.com", 1883, 60)
-client.subscribe("casa/movimiento")
-client.loop_start()
+        
 
-# Mostrar estado actual
-st.markdown(st.session_state.movimiento)
+
+broker="157.230.214.127"
+port=1883
+client1= paho.Client("GIT-HUB")
+client1.on_message = on_message
+
+
+
+st.title("MQTT Control")
+
+if st.button('ON'):
+    act1="ON"
+    client1= paho.Client("GIT-HUB")                           
+    client1.on_publish = on_publish                          
+    client1.connect(broker,port)  
+    message =json.dumps({"Act1":act1})
+    ret= client1.publish("cmqtt_s", message)
+ 
+    #client1.subscribe("Sensores")
+    
+    
+else:
+    st.write('')
+
+if st.button('OFF'):
+    act1="OFF"
+    client1= paho.Client("GIT-HUB")                           
+    client1.on_publish = on_publish                          
+    client1.connect(broker,port)  
+    message =json.dumps({"Act1":act1})
+    ret= client1.publish("cmqtt_s", message)
+  
+    
+else:
+    st.write('')
+
+values = st.slider('Selecciona el rango de valores',0.0, 100.0)
+st.write('Values:', values)
+
+if st.button('Enviar valor analógico'):
+    client1= paho.Client("GIT-HUB")                           
+    client1.on_publish = on_publish                          
+    client1.connect(broker,port)   
+    message =json.dumps({"Analog": float(values)})
+    ret= client1.publish("cmqtt_a", message)
+    
+ 
+else:
+    st.write('')
+
+
+
